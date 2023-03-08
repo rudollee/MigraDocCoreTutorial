6d856efdeef3d6aab7f1f25fdc3f2db054ba0311
wook@desktop05 <rudollee@gmail.com>
2023-03-08 20:43:14 +0900

    template improved

--- MigraDocCoreTutorial/Controllers/HomeController.cs
+++ MigraDocCoreTutorial/Controllers/HomeController.cs
@@ -4,6 +4,8 @@ using MigraDocCore.DocumentObjectModel;
 using MigraDocCore.Rendering;
 using MigraDocCoreTutorial.Models;
 using System.Diagnostics;
+using PdfSharpCore.Pdf;
+using PdfSharpCore.Drawing;
 
 namespace MigraDocCoreTutorial.Controllers
 {
@@ -57,41 +59,80 @@ namespace MigraDocCoreTutorial.Controllers
             #endregion
 
             // We start with a new document:
-            var document = new Document();
+            var fontName = "Malgun Gothic";
+            var doc = new PdfDocument();
+
+            var page = doc.AddPage();
+            var gfx = XGraphics.FromPdfPage(page);
+            gfx.MUH = PdfFontEncoding.Unicode;
+
+            Document document = new Document();
+            Unit margin = Unit.FromPoint(50);
+            document.DefaultPageSetup.TopMargin = margin;
+            document.DefaultPageSetup.BottomMargin = margin;
+            document.DefaultPageSetup.LeftMargin = margin;
+            document.DefaultPageSetup.RightMargin = margin;
+
+            var docRenderer = new DocumentRenderer(document);
 
             // With MigraDocCore, we don’t add pages, we add sections (at least one):
             var section = document.AddSection();
 
-            // Adding text is simple:
-            section.AddParagraph("Fruits");
+            #region 타이틀
+            var paragraph = section.AddParagraph();
+            paragraph.Format.Alignment = ParagraphAlignment.Center;
+            paragraph.Format.Font.Name = fontName;
+            paragraph.Format.Font.Size = Unit.FromPoint(16);
+            //para.Format.Borders.Top.Color = Colors.Black;
+            //para.Format.Shading.Color = Colors.LightGray;
+            paragraph.Format.Borders.Bottom.Color = Colors.Black;
+            paragraph.Format.Borders.DistanceFromTop = Unit.FromPoint(5);
+            paragraph.Format.Borders.DistanceFromBottom = Unit.FromPoint(10);
 
-            // Store the newly created object for modification:
-            Paragraph paragraph = section.AddParagraph();
             paragraph.Format.Font.Color = Color.FromCmyk(100, 30, 20, 50);
-            paragraph.Format.Font.Name = "Malgun Gothic";
-            paragraph.AddFormattedText("과일 재고", TextFormat.Underline);
+            paragraph.AddFormattedText($"과일 재고", TextFormat.Bold);
+            #endregion
+
 
             // AddFormattedText also returns an object:
             FormattedText ft = paragraph.AddFormattedText("really fressh!!", TextFormat.Bold);
             ft.Font.Size = 6;
 
+            paragraph = section.AddParagraph();
+            paragraph.Format.Alignment = ParagraphAlignment.Left;
+            paragraph.Format.Font.Name = fontName;
+            paragraph.Format.Font.Size = Unit.FromPoint(14);
+            paragraph.Format.Borders.Top.Color = Colors.LightGray;
+            paragraph.Format.Borders.DistanceFromTop = Unit.FromPoint(20);
+            paragraph.Format.Borders.DistanceFromBottom = Unit.FromPoint(5);
+            paragraph.AddText("오늘의 과일");
+
             // Adding empty paragraphs is even simpler:
             section.AddParagraph();
 
             var table = section.AddTable();
-            table.AddColumn(Unit.FromPoint(100));
-            table.AddColumn(Unit.FromPoint(100));
-            table.AddColumn(Unit.FromPoint(100));
-            table.AddColumn(Unit.FromPoint(100));
+            table.Borders.DistanceFromTop = 100;
+            table.TopPadding = 5;
+            table.BottomPadding = 5;
+            table.AddColumn(Unit.FromPoint(125));
+            table.AddColumn(Unit.FromPoint(125));
+            table.AddColumn(Unit.FromPoint(125));
+            table.AddColumn(Unit.FromPoint(125));
 
             Row r;
 
             // Title
             r = table.AddRow();
+            r.Height = 20;
             r.Cells[0].AddParagraph("Name");
             r.Cells[1].AddParagraph("Units");
             r.Cells[2].AddParagraph("Amount");
             r.Cells[3].AddParagraph("Price");
+            foreach (Cell cell in r.Cells)
+            {
+                cell.Format.Alignment = ParagraphAlignment.Right;
+            }
+            r.Cells[0].Format.Alignment = ParagraphAlignment.Left;
 
             // Data
             fruits.ForEach(fruit =>
@@ -104,7 +145,10 @@ namespace MigraDocCoreTutorial.Controllers
                 foreach (Cell cell in r.Cells)
                 {
                     cell.Format.Font.Name = "Segoe UI";
+                    cell.Format.Alignment = ParagraphAlignment.Right;
                 }
+
+                r.Cells[0].Format.Alignment = ParagraphAlignment.Left;
             });
 
             // With MigraDocCore you can create PDF or RTF. Just select the appropriate renderer:
@@ -114,11 +158,12 @@ namespace MigraDocCoreTutorial.Controllers
             pdfRenderer.Document = document;
 
             // Let the renderer do its job:
-            pdfRenderer.RenderDocument();
+            docRenderer.PrepareDocument();
+            docRenderer.RenderPage(gfx, 1);
 
             // Save the PDF to a file:
             var stream = new MemoryStream();
-            pdfRenderer.PdfDocument.Save(stream);
+            doc.Save(stream);
 
             return File(stream.ToArray(), "application/pdf", "Fruits.pdf");
         }
